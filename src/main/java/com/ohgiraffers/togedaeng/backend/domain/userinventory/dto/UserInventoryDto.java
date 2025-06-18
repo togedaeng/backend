5a81069e28730d63bf6aa8b118b75131459f4379
yunnzi040 <zeongyunzi@gmail.com>
2025-06-18 15:21:17 +0900

    OPS-7: Jira 이슈 키 포함 테스트

--- src/main/java/com/ohgiraffers/togedaeng/backend/domain/userinventory/dto/UserInventoryDto.java
+++ src/main/java/com/ohgiraffers/togedaeng/backend/domain/userinventory/dto/UserInventoryDto.java
@@ -2,10 +2,12 @@ package com.ohgiraffers.togedaeng.backend.domain.userinventory.dto;
 
 import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
