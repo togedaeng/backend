cc49ab5538a29267a7c653389a8933d20c8af255
yunnzi040 <zeongyunzi@gmail.com>
2025-06-18 16:39:40 +0900

    OPS-8: jira 이슈 발행 테스트

--- src/main/java/com/ohgiraffers/togedaeng/backend/domain/userinventory/dto/UserInventoryDto.java
+++ src/main/java/com/ohgiraffers/togedaeng/backend/domain/userinventory/dto/UserInventoryDto.java
@@ -4,11 +4,9 @@ import java.time.LocalDateTime;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @AllArgsConstructor
-@NoArgsConstructor
 @Getter
 @Setter
 public class UserInventoryDto {
