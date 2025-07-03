afed5d16c1bfe84972b2ad207dd910164bdf9444
gaeun2593@gmail.com <gaeun2593@gmail.com>
2025-07-03 18:21:00 +0900

    remove: 이전 강아지 도메인 삭제

--- src/main/java/com/ohgiraffers/togedaeng/backend/domain/dog/repository/DogImageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ohgiraffers.togedaeng.backend.domain.dog.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ohgiraffers.togedaeng.backend.domain.dog.entity.DogImage;
-
-public interface DogImageRepository extends JpaRepository<DogImage, Long> {
-}
