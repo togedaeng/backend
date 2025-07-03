15420c5e4ed1fd232c99e765cfa2ed6edcf513b7
gaeun2593@gmail.com <gaeun2593@gmail.com>
2025-07-03 14:51:40 +0900

    feat: 강아지 등록 반환 dto 추가

---
+++ src/main/java/com/ohgiraffers/togedaeng/backend/domain/Ndog/dto/response/CreateDogResponseDto.java
@@ -0,0 +1,28 @@
+package com.ohgiraffers.togedaeng.backend.domain.Ndog.dto.response;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import com.ohgiraffers.togedaeng.backend.domain.Ndog.entity.Gender;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CreateDogResponseDto {
+
+	private Long id;
+	private Long userId;
+	private Long personalityCombinationId;
+	private String name;
+	private Gender gender;
+	private LocalDate birth;
+	private String callName;
+	private LocalDateTime createdAt;
+
+}
