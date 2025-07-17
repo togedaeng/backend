a0713e3b11cf2f68c2d291fff684d984afd2b76a
dansun2 <dansunmusik7@gmail.com>
2025-07-17 10:14:44 +0900

    feat: add ImageValidationError

---
+++ src/main/java/com/ohgiraffers/togedaeng/backend/domain/custom/entity/ImageValidationError.java
@@ -0,0 +1,9 @@
+package com.ohgiraffers.togedaeng.backend.domain.custom.entity;
+
+public enum ImageValidationError {
+	NOT_DOG_IMAGE,
+	LOW_RESOLUTION,
+	POLICY_VIOLATION,
+	INAPPROPRIATE_CONTENT,
+	PROCESSING_ERROR,
+}
